3d2abba7fba47f9486f4624f5798ef8a157b4c3f
Fabiola <f.fuentes.baeza@gmail.com>
2022-09-07 13:20:18 -0300

    avances 3

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -32,8 +32,9 @@ const MiApi = () => {
      return (
       <>
         <header>
-           <h1> Feriados de Chile Año 2022</h1>
-           {/* <img src={ bandera } alt="" className="bandera"/>  */}
+          <img src="https://assets.stickpng.com/images/580b585b2edbce24c47b27f2.png" alt="" className="bandera"/> 
+          <h1> Feriados de Chile Año 2022</h1>
+          <img src="https://assets.stickpng.com/images/580b585b2edbce24c47b27f2.png" alt="" className="bandera"/>
            {/* <img className="bandera" src={"public/bandera.png"} /> */}
         </header>
         <main>
@@ -100,10 +101,9 @@ const MiApi = () => {
 
           {}
         </section>
-
         
     </>
-     )
+    )
  
 }
 
