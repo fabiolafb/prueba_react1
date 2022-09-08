bb09a57b7b9fa393562d9783c655a89fbff33ef0
Fabiola <f.fuentes.baeza@gmail.com>
2022-09-08 14:56:14 -0300

    avance final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,8 @@ const Header = () => {
   return (
     <header>
         <img src="https://assets.stickpng.com/images/580b585b2edbce24c47b27f2.png" alt="" className="bandera"/> 
-        <h1> Feriados de Chile Año 2022</h1>
+        <h1> Feriados de Chile año 2022</h1>
         <img src="https://assets.stickpng.com/images/580b585b2edbce24c47b27f2.png" alt="" className="bandera"/>
-        {/* <img className="bandera" src={"public/bandera.png"} /> */}
     </header>
   )
 }
--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -4,51 +4,50 @@ moment.locale('es')
 
  
 const MiApi = () => {
-    const [listaFeriados, setListaFeriados] = useState([])
-    // Asignar estado búsqueda
-    const [buscando, setBuscando] = useState("")
-    // Asignar estado filtro
-    const [filtradoFeriados, setFiltradoFeriados] = useState();
+  //Se asigna estado para recibir array
+  const [listaFeriados, setListaFeriados] = useState([]);
+  // Asignar estado búsqueda
+  const [buscando, setBuscando] = useState("");
+  // Asignar estado filtro
+  const [filtradoFeriados, setFiltradoFeriados] = useState("");
 
-    //Usar hook useEffect
-    useEffect(() => {
+    //Usar el hook useEffect para modificar
+  useEffect(() => {
     obtenerFeriados();
-     }, []);
+  }, []);
     
      //Función que consulta la API
-    const obtenerFeriados = async () => {
-       const url = 'https://api.victorsanmartin.com/feriados/en.json';
-       const response = await fetch(url)
-       const {data} = await response.json()
-       //console.log(data)
-       setListaFeriados(data).reverse();
-    }
-   
-    const capturaExtraFeriados = function(e) {
-      const opcion = e.target.value;
-      setFiltradoFeriados(opcion).reverse();
-   }
+  const obtenerFeriados = async () => {
+    const url = 'https://api.victorsanmartin.com/feriados/en.json';
+    const response = await fetch(url)
+    const { data } = await response.json()
+    //console.log(data)
+    setListaFeriados(data);
+  };
+  const capturaExtraFeriados = function (e) {
+    const opcion = e.target.value;
+    setFiltradoFeriados(opcion);
+  };   
 
-    
-     return (
-      <>
-        <main>
-          {/* Seccion filtrado */}
+return (
+  <>
+    <main>
+      {/* Seccion filtrado */}
+      <div className="buscador_div">  
+        <h4 className="titulo">Buscador de feriado</h4>
+        <input      
+          type="text"
+          className="buscador_input"
+          placeholder="Buscar feriado"
+          value={buscando}
+          onChange={(e) => {
+            setBuscando(e.target.value);
+          }}
+        />
+        </div>
           <div className="buscador_div">  
-            <h3 className="titulo">Buscador de feriado</h3>
-            <input      
-              type="text"
-              className="buscador_input"
-              placeholder="Buscar feriado"
-              value={buscando}
-              onChange={(e) => {
-                setBuscando(e.target.value)}
-              }
-            />
-          </div>
-          <div className="buscador_div">  
-            <h3 className="titulo">Filtrar feriado</h3>
-            <select className="buscador_input" id="date" >
+            <h4 className="titulo">Filtrar por tipo feriado</h4>
+            {/* <select className="buscador_input" id="date" >
               <option value={-1}> <i>Mes del año</i> </option>
               <option value="Enero">Enero</option>
               <option value="Febrero">Febrero</option>
@@ -62,42 +61,60 @@ const MiApi = () => {
               <option value="Octubre">Octubre</option>
               <option value="Noviembre">Noviembre</option>
               <option value="Diciembre">Diciembre</option>
-            </select>
+            </select> */}
 
-            <select className="buscador_input" id="extra" onClick={capturaExtraFeriados}>
-              <option value={-1}>Tipo de feriado</option>
+            <select 
+              className="buscador_input" 
+              id="type" 
+              onChange={(e) => setFiltradoFeriados(e.target.value)}
+              value={filtradoFeriados}
+            >
+              <option value=""></option>
               <option value="Civil">Civil</option>
               <option value="Religioso">Religioso</option>
-              <option value="Irrenunciable">Irrenunciable</option>
-             </select>
+            </select> 
           </div>
         </main>
 
         {/* Seccion cards */}
         <section>
-            {listaFeriados.filter((lista) => {
-          if (buscando === "") {
-            return lista;
-          } else if (lista.title.toLocaleLowerCase().includes(buscando.toLocaleLowerCase()) ||
-            lista.date.toLocaleLowerCase().includes(buscando.toLocaleLowerCase()) ||
-            lista.extra.toLocaleLowerCase().includes(buscando.toLocaleLowerCase()) ) {
-            return lista;
-          };
+          {listaFeriados.filter((lista) => {
+            if (buscando === "" && filtradoFeriados === "")
+            {
+              return lista;
+            } else if (
+              buscando === "" &&
+              lista.type.includes(filtradoFeriados)
+            ) {
+              return lista;
+            } else if (
+               filtradoFeriados === "" && 
+               (lista.title
+                  .toLocaleLowerCase()
+                  .includes(buscando.toLocaleLowerCase()) ||
+                lista.date
+                  .toLocaleLowerCase()
+                  .includes(buscando.toLocaleLowerCase()) ||
+                lista.type
+                  .toLocaleLowerCase()
+                  .includes(buscando.toLocaleLowerCase()))
+             ) {
+              return lista;
+            }
           
-          }).map(f => 
-          <div className="card" key={f} >
-            <div className="col p-1"  >
-              <h5 className="card-title"><b>{f.title}</b> </h5>
-              <h5 className="card-subtitle mb-1" id="fecha"> Fecha: {f.date}</h5>
-              <p className="card-text" >Tipo de feriado: <b>{f.extra}</b> </p>
+          }).map((f, i) => (
+            <div className="card" key={i} >
+              <div className="col p-1"  >
+                <h5 className="card-title"><b>{f.title}</b>{""} </h5>
+                <h5 className="card-subtitle mb-1">  Fecha: {f.date}{" "}</h5>
+                <p className="card-text" >Tipo de feriado: <b>{f.type}</b> {" "} </p>
+              </div>
             </div>
-          </div>
-          )}
-          {}
-        </section>
-      </>
-    )
- 
-}
+          ))}
+        {}
+      </section>
+    </>
+  );
+};
 
 export default MiApi
